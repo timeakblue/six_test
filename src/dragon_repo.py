c93bfc001519e3862cb90547613671abb1c40d55
timeakblue <timeakblue@gmail.com>
2025-10-18 02:53:52 -0700

    Update dragon_repo.py
    adding class

--- src/dragon_repo.py
+++ src/dragon_repo.py
@@ -2,3 +2,23 @@
 
 from dataclasses import dataclass, field
 from typing import Dict, List, Optional
+
+
+
+class RocketStatus(str, Enum):
+  
+class MissionStatus(str, Enum):
+  
+class RepoError(Exception):
+  
+class NotFoundError(RepoError):
+  
+class ValidationError(RepoError):
+  
+@dataclass
+class Rocket:
+  
+@dataclass
+class Mission:
+  
+class RocketsRepository:
